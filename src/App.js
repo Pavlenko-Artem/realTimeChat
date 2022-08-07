bb2a63d5ac31889545aa5cd3b12d57f0e4d93f91
Artem Pavlenko <artempavlenko1703@gmail.com>
2022-08-07 15:39:45 +0300

    Add BrowserRouter in App, add consts publicRoutes, privateRoutes in routes,js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
 
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,17 @@
-export const publicRoutes = [
+import Login from "./components/Login";
+import Chat from "./components/Chat";
+import { CHAT_ROUTE, LOGIN_ROUTE } from "./utils/const";
 
+export const publicRoutes = [
+    {
+        path: LOGIN_ROUTE,
+        Component: Login
+    }
+]
+
+export const privateRoutes = [
+    {
+        path: CHAT_ROUTE,
+        Component: Chat
+    }
 ]
